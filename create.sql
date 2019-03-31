3c13fa6b598b80a7e49841b531ab4573b4ddb418
Aaron <aaron@theburntcity.com>
2019-03-31 11:40:42 -0700

    Create table sql file

---
+++ create.sql
@@ -0,0 +1,16 @@
+create table peps (
+	id INT not null,
+	first_name VARCHAR(150) not null,
+	last_name VARCHAR(150) not null,
+	email VARCHAR(150) not null,
+	gender VARCHAR(150) not null,
+	ip_address VARCHAR(120) not null ,
+	favorite_animal VARCHAR(150) not null,
+	avataro VARCHAR(150) not null,
+	employer VARCHAR(150) not null,
+	address VARCHAR(150) not null,
+	city VARCHAR(150) not null,
+	state VARCHAR(150) not null,
+	zip VARCHAR(150) not null,
+	buzz VARCHAR(150) not null
+);
